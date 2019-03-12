037839c17e765ffc7b83f95e82a7ebc919c7552e
Shaun Wakashige <theswak@gmail.com>
2019-03-12 07:50:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if(string.toUpperCase()) {
-    return "I love you, Grandma."
+    return "I love you, too."
   } else if (string.toLowerCase()) {
     return "I can\'t hear you!"
   } else {
